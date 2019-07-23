8a0898f513a75ebea267426db271a3e957af8998
SafetyMarcus <hooperarma@gmail.com>
2019-07-24 08:27:53 +1000

    Adding splash screen to hide theme changes

---
+++ app/src/main/java/com/safetymarcus/portfolio/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.safetymarcus.portfolio
+
+import android.content.Intent
+import android.os.Bundle
+import com.safetymarcus.portfolio.utils.CoroutineActivity
+
+/**
+ * @author Marcus Hooper
+ */
+class SplashActivity : CoroutineActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
